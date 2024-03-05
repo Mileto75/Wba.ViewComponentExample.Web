a9ce0fb9616d2b7fbfea50e2b648c795dc412ea4
Mileto Di Marco <mileto.di.marco@howest.be>
2024-03-05 14:41:35 +0100

    Add chuck norris viewcomponent

---
+++ Wba.ViewComponentExample.Web/ViewComponents/ChuckNorrisViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Wba.ViewComponentExample.Web.ViewModels;
+
+namespace Wba.ViewComponentExample.Web.ViewComponents
+{
+    [ViewComponent(Name = "QuoteComponent")]
+    public class ChuckNorrisViewComponent : ViewComponent
+    {
+        private const string QuoteUrl = "https://api.chucknorris.io/jokes/random";
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(QuoteUrl);
+            var result = await response.Content.ReadAsStringAsync();
+            var quoteComponentViewModel =
+                JsonConvert.DeserializeObject<QuoteComponentViewModel>(result);
+            return View(quoteComponentViewModel);
+        }
+    }
+}
---
+++ Wba.ViewComponentExample.Web/ViewModels/QuoteComponentViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Wba.ViewComponentExample.Web.ViewModels
+{
+    public class QuoteComponentViewModel
+    {
+        public string Value { get; set; }
+    }
+}
